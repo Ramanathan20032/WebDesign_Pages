57c9dc23b6b522ecb0181019c8521ff3487b4349
Ramanathan20032 <Ramanathan20032@gmail.com>
2024-12-23 15:32:35 +0530

    initial commit

---
+++ Random_PasswordGenerator/index.js
@@ -0,0 +1,15 @@
+const generateBtn = document.querySelector('button')
+const password = document.getElementById("password")
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789"
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+function createPassword(){
+    let password = ""
+    password += password[Math.floor(Math.ra)]
+}
+
+generateBtn.addEventListener('click', createPassword);
